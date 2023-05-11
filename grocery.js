61d73b286427f786084b44cfc60e4b7e444397e1
tsopsericado <ricardotsopse@gmail.com>
2023-05-11 07:00:46 +0100

    some features added in the html and grocery.js files

--- grocery.js
+++ grocery.js
@@ -1,7 +1,21 @@
 //initialising empty array to store list items 
 
 var groceryList = 0;
-
+ 
+// get input value
 function addItems() {
-    var item = document.getElementById("item").ariaValueMax;
+    var item = document.getElementById("item").value;
+    
+    // checking if input is not empty
+    if (item !==""){
+    
+        // add item to array    
+        groceryList.push(item);
+        
+        // clair input field
+        document.getElementById("item").value ="";
+        
+        // display update
+        displayList();
+    }
 }
