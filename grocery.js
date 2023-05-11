c424adbbc5201baa59080b758f92f9c2db31fcc9
tsopsericado <ricardotsopse@gmail.com>
2023-05-11 15:27:47 +0100

    project under construction

--- grocery.js
+++ grocery.js
@@ -1,47 +1,13 @@
-//initialising empty array to store list items
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-var groceryList = 0;
+//select all the initial lis
+var lis = document.querySelectorAll("li");
+//  create a variable for future delete buttons
+var deleteButton;
 
-// get input value
-function addItems() {
-  var item = document.getElementById("item").value;
-
-  // checking if input is not empty
-  if (item !== "") {
-    // add item to array
-    groceryList.push(item);
-
-    // clair input field
-    document.getElementById("item").value = "";
-
-    // display update
-    displayList();
-  }
+//Return Length of input
+function inputLenght(){
+    return
 }
-
-// Initialising unordered list tag
-
-function displayList() {
-  var list = "<ul>"; 
-  for (var i = 0; i < groceryList.length; i++) {
-    list +=
-      "<li>" +
-      groceryList[i] +
-      " <button onclick='editItem(" +
-      i +
-      ")'>Edit</button> <button onclick='deleteItem(" +
-      i +
-      ")'>Delete</button></li>";
-    // add list item with edit and delete buttons
-  }
-    //close unordered list tag
-   list += "</ul>"
-   //display list on page 
-   document.getElementById("list").innerHTML = list;
-}
-  function deleteItem(index) {
-    // remove item from array at specified index
-    groceryList.splice(index, 1);
-    // display updated list
-    displayList();
-  }
